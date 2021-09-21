a25e25b963a59ef9f7f9ae2c35ba7681f909709a
Matheus Linhaus <matheuslinhauss@hotmail.com>
2021-09-21 18:57:29 -0300

    Adicionado icone no botão de download, deixar informações mais legiveis
    Adicionado um icone para o botão de download, e também alterado a chamada do download do arquivo e deixar as informações mais legiveis

--- downloader.py
+++ downloader.py
@@ -5,26 +5,31 @@ import re
 import unicodedata
 import tela
 
-def yt_download():
-    # download a file with only audio, to save space
-    # if the final goal is to convert to mp3
-    try:
-        tgt_folder = "C:/MP3/"
-        youtube_link = tela.url_text.get('1.0', 'end')
-        y = YouTube(youtube_link)
-        t = y.streams.filter(only_audio=True).all()
-        name = removerAcentosECaracteresEspeciais(y.title).replace("  ", " ")
-        t[0].download(output_path=tgt_folder, filename_prefix=name)
-        for file in [n for n in os.listdir(tgt_folder) if re.search('mp4', n)]:
-            full_path = os.path.join(tgt_folder, file)
+diretorio = "C:/MP3/"
+
+def baixa_arquivo_youtube():
+    dados_video = YouTube(tela.url_text.get('1.0', 'end'))
+    download_video = dados_video.streams.filter(only_audio=True).all()
+    titulo_video = removerAcentosECaracteresEspeciais(dados_video.title).replace("  ", " ")
+    download_video[0].download(output_path=diretorio, filename_prefix=titulo_video)
+
+def converte_arquivo_mp3():
+    for arquivo in [n for n in os.listdir(diretorio) if re.search('mp4', n)]:
+            full_path = os.path.join(diretorio, arquivo)
             output_path = os.path.join(
-                tgt_folder, os.path.splitext(file)[0] + '.mp3')
-            clip = mp.AudioFileClip(full_path).subclip(10,)
+                diretorio, os.path.splitext(arquivo)[0] + '.mp3')
+            clip = mp.AudioFileClip(full_path).subclip()
             clip.write_audiofile(output_path)
-            os.remove(tgt_folder+file)
+            os.remove(diretorio+arquivo)
+
+def download_button():
+    tela.texto_mensagem.grid(column=1, row=2, pady=(0, 6))
+    try:
+        baixa_arquivo_youtube()
+        converte_arquivo_mp3()
+        tela.texto_mensagem.configure(text="Download concluído com sucesso!!", fg='#008000', font=('Arial', 9, 'bold'))
     except:
-        erro = tela.Label(tela.janela, text='Informe uma URL do YouTube para download', fg='#f00', font=('Arial', 9))
-        erro.grid(column=1, row=2, pady=(0, 6))
+        tela.texto_mensagem.configure(text="Informe uma URL válida do YouTube para download", fg='#f00', font=('Arial', 9, 'bold'))
 
 # # A remoção de acentos foi baseada em uma resposta no Stack Overflow.
 # # http://stackoverflow.com/a/517974/3464573
--- tela.py
+++ tela.py
@@ -1,6 +1,5 @@
 from tkinter import *
-from tkinter.ttk import Progressbar
-from downloader import *
+import downloader
 
 janela = Tk()
 
@@ -11,17 +10,21 @@ imagem = imagem.subsample(23)
 logo = Label(janela, image=imagem)
 logo.grid(column=1, row=0, pady=(10, 0))
 
-
 texto_descricao = Label(janela, text='')
 texto_descricao.grid(column=0, row=0)
 
-texto_url = Label(janela, text='URL: ', font=('Helvetica', 11, 'bold'))
+texto_url = Label(janela, text='URL: ', font=('Arial', 10, 'bold'))
 texto_url.grid(column=0, row=1, sticky=W)
 
 url_text = Text(janela, height=1, width=50)
 url_text.grid(column=1, row=1, sticky=W, padx=(5, 8), pady=10)
 
-botao_download = Button(janela, text='Download', command=yt_download)
+texto_mensagem = Label(janela, text="")
+
+photo = PhotoImage(file = "icon-download.png")
+photoimage = photo.subsample(30, 30)
+
+botao_download = Button(janela, text='Download', command=downloader.download_button, image = photoimage, compound='left')
 botao_download.grid(column=1, row=3, pady=(0, 6))
 
 janela.mainloop()
