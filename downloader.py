8333d945ac5b4a5c06253bb0aef45552f224337e
Matheus Linhaus <matheuslinhauss@hotmail.com>
2021-09-21 17:24:30 -0300

    Alterações no processo, e incluir logo
    Alterado o processo com try, para melhorar como é feito o donwload, caso tenha erro será informando um erro

--- downloader.py
+++ downloader.py
@@ -8,24 +8,31 @@ import tela
 def yt_download():
     # download a file with only audio, to save space
     # if the final goal is to convert to mp3
-    tgt_folder = "C:/MP3/"
-    youtube_link = tela.url_text.get('1.0', 'end')
-    y = YouTube(youtube_link)
-    t = y.streams.filter(only_audio=True).all()
-    name = removerAcentosECaracteresEspeciais(y.title).replace("  ", " ")
-    t[0].download(output_path=tgt_folder, filename_prefix=name)
-    for file in [n for n in os.listdir(tgt_folder) if re.search('mp4',n)]:
-        full_path = os.path.join(tgt_folder, file)
-        output_path = os.path.join(tgt_folder, os.path.splitext(file)[0] + '.mp3')
-        clip = mp.AudioFileClip(full_path).subclip(10,)
-        clip.write_audiofile(output_path)
-        os.remove(tgt_folder+file)
+    try:
+        tgt_folder = "C:/MP3/"
+        youtube_link = tela.url_text.get('1.0', 'end')
+        y = YouTube(youtube_link)
+        t = y.streams.filter(only_audio=True).all()
+        name = removerAcentosECaracteresEspeciais(y.title).replace("  ", " ")
+        t[0].download(output_path=tgt_folder, filename_prefix=name)
+        for file in [n for n in os.listdir(tgt_folder) if re.search('mp4', n)]:
+            full_path = os.path.join(tgt_folder, file)
+            output_path = os.path.join(
+                tgt_folder, os.path.splitext(file)[0] + '.mp3')
+            clip = mp.AudioFileClip(full_path).subclip(10,)
+            clip.write_audiofile(output_path)
+            os.remove(tgt_folder+file)
+    except:
+        erro = tela.Label(tela.janela, text='Informe uma URL do YouTube para download', fg='#f00', font=('Arial', 9))
+        erro.grid(column=1, row=2, pady=(0, 6))
 
 # # A remoção de acentos foi baseada em uma resposta no Stack Overflow.
 # # http://stackoverflow.com/a/517974/3464573
+
 def removerAcentosECaracteresEspeciais(palavra):
-        # Unicode normalize transforma um caracter em seu equivalente em latin.
-        nfkd = unicodedata.normalize('NFKD', palavra)
-        palavraSemAcento = u"".join([c for c in nfkd if not unicodedata.combining(c)])
-        # Usa expressão regular para retornar a palavra apenas com números, letras e espaço
-        return re.sub('[^a-zA-Z0-9 \\\]', '', palavraSemAcento)
+    # Unicode normalize transforma um caracter em seu equivalente em latin.
+    nfkd = unicodedata.normalize('NFKD', palavra)
+    palavraSemAcento = u"".join(
+        [c for c in nfkd if not unicodedata.combining(c)])
+    # Usa expressão regular para retornar a palavra apenas com números, letras e espaço
+    return re.sub('[^a-zA-Z0-9 \\\]', '', palavraSemAcento)
--- tela.py
+++ tela.py
@@ -1,21 +1,27 @@
 from tkinter import *
+from tkinter.ttk import Progressbar
 from downloader import *
 
 janela = Tk()
 
 janela.title('YouTube Download')
 
-texto_descricao = Label(janela, text='Insira a URL para download')
-texto_descricao.grid(column=0 , row=0)
+imagem = PhotoImage(file="youtube-logo-9-2048x456.png")
+imagem = imagem.subsample(23)
+logo = Label(janela, image=imagem)
+logo.grid(column=1, row=0, pady=(10, 0))
 
-texto_url = Label(janela, text='URL: ')
-texto_url.grid(column=0, row=1)
 
-url_text = Text(janela,height = 1,width = 23)
-url_text.grid(column=1, row=1)
+texto_descricao = Label(janela, text='')
+texto_descricao.grid(column=0, row=0)
 
+texto_url = Label(janela, text='URL: ', font=('Helvetica', 11, 'bold'))
+texto_url.grid(column=0, row=1, sticky=W)
+
+url_text = Text(janela, height=1, width=50)
+url_text.grid(column=1, row=1, sticky=W, padx=(5, 8), pady=10)
 
 botao_download = Button(janela, text='Download', command=yt_download)
-botao_download.grid(column=1, row=2)
+botao_download.grid(column=1, row=3, pady=(0, 6))
 
 janela.mainloop()
